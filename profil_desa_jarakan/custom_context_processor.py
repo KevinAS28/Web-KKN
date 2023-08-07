2344b34ce1c51f663a553b09906320932da6e3fe
Kevin Agusto <kevinagusto28@gmail.com>
2023-08-07 18:38:30 +0700

    change footer does not have to restart

--- profil_desa_jarakan/custom_context_processor.py
+++ profil_desa_jarakan/custom_context_processor.py
@@ -1,10 +1,11 @@
 from django.conf import settings
 
 import json, os
-desa_info = json.loads(open(os.path.join(settings.BASE_DIR, 'info_desa.json')).read())
+
 
 
 def custom_variables(request):
+    desa_info = json.loads(open(os.path.join(settings.BASE_DIR, 'info_desa.json')).read())
     return {
         'nama_desa': desa_info['nama_desa'],
         'email_desa': desa_info['email_desa'],
